ff2a366a4ea4302c50b7ed585fc307af945b3b79
hntoma <hntoma@gmail.com>
2021-06-21 22:22:56 -0700

    first commit

---
+++ History.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+<title>Unit Converter - Length</title>
+</head>
+
+<body>
+<form action="length.php" method="POST">
+    
+<p style="color:red;">Page 3 [History] </p>
+<p style="color:red;">Converter Site</p>
+1. <a href="http://localhost/lab/home.php">Home</a></a></h1> 2.<a href="http://localhost/lab/conversion.php">Convertion site</a></a></h1> 3. <a href="http://localhost/lab/history.php">History</a></a></h1>  
+
+<p style="color:red;">Conversion History: </p>
---
+++ conversion.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+<title>Unit Converter - Length</title>
+</head>
+
+<body>
+<form action="length.php" method="POST">
+    
+<p style="color:red;">Page 2 [Conversion Rate] </p>
+<p style="color:red;">Converter Site</p>
+1. <a href="http://localhost/Webtech/home.php">Home</a></a></h1> 2.<a href="http://localhost/Webtech/conversion.php">Convertion site</a></a></h1> 3. <a href="http://localhost/Webtech/history.php">History</a></a></h1>  
+
+<p style="color:red;">Conversion Rate: </p>
